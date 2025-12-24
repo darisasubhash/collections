ed2e9990e64516326d1a6598569820bee0e56973
darisasubhash <subbusubbu8193@gmail.com>
2025-12-24 16:14:37 +0530

    changing the linkedlist

--- src/com/collections/list/ArrayListImplementation.java
+++ src/com/collections/list/ArrayListImplementation.java
@@ -1,11 +1,9 @@
 package com.collections.list;
-
 import java.util.ArrayList;
 
 public class ArrayListImplementation {
-   ArrayList list=new ArrayList();
-
-   {
+   public static void main(String[] args) {
+      ArrayList<Integer> list=new ArrayList<>();
       list.add(20);
       list.add(100);
       list.add(200);
@@ -14,17 +12,17 @@ public class ArrayListImplementation {
       list.add(90);
       list.add(1,20);
 
-      ArrayList array1=new ArrayList<>();
+      ArrayList<Integer> array1=new ArrayList<>();
       array1.add(10);
       array1.add(null);
       array1.add(2,200);
       array1.addFirst(20);
       array1.addLast(20);
-      array1.hashCode();
+      System.out.println(array1.hashCode());
       list.addAll(array1);
       list.addAll(1,array1);
       Object []array=list.toArray();
-      list.stream().forEach(System.out::println);
+      list.forEach(System.out::println);
       list.set(3,45);
       System.out.println(list.indexOf(25));
       System.out.println(list.lastIndexOf(35));
@@ -34,11 +32,5 @@ public class ArrayListImplementation {
       array1.trimToSize();
       System.out.println(array1.get(5));
       System.out.println(array1.size());
-
-
-
-
    }
-
-
 }
