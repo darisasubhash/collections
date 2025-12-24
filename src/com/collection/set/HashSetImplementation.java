181eb4ea02f6ff88fd957c4e042cb7c5aa755c44
darisasubhash <subbusubbu8193@gmail.com>
2025-12-24 12:08:41 +0530

    addding hashset

---
+++ src/com/collection/set/HashSetImplementation.java
@@ -0,0 +1,31 @@
+package com.collection.set;
+
+import java.util.HashSet;
+
+public class HashSetImplementation {
+
+    {
+        HashSet set=new HashSet();
+
+        set.add(10);
+        set.add(30);
+        set.add(20);
+        set.add(10);
+        set.add(null);
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+        System.out.println(set.contains(20));
+        set.remove(30);
+        HashSet<Integer> set2 = new HashSet<>();
+        set2.add(50);
+        set2.add(20);
+        set.addAll(set2);
+        set.removeAll(set2);
+        set2.add(30);
+        set2.add(45);
+        set2.add(60);
+        set.retainAll(set2);
+        set.clear();
+
+    }
+}
