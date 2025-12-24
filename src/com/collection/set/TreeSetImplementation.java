29c5c113016fb3814c9bc392492137edd89212c9
darisasubhash <subbusubbu8193@gmail.com>
2025-12-24 16:32:29 +0530

    updating the treeset

--- src/com/collection/set/TreeSetImplementation.java
+++ src/com/collection/set/TreeSetImplementation.java
@@ -1,11 +1,9 @@
 package com.collection.set;
-
 import java.util.TreeSet;
 
 public class TreeSetImplementation {
-
-    TreeSet set=new TreeSet();
-    {
+    public static void main(String[] args) {
+        TreeSet<Integer> set=new TreeSet<>();
         set.add(40);
         set.add(10);
         set.add(30);
