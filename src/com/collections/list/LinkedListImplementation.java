e2e79ccf5f58dd3e4b3963af1ce17bfec16c7c2a
darisasubhash <subbusubbu8193@gmail.com>
2025-12-24 11:43:43 +0530

    adding linkedlist

---
+++ src/com/collections/list/LinkedListImplementation.java
@@ -0,0 +1,42 @@
+package com.collections.list;
+
+import java.util.LinkedList;
+
+public class LinkedListImplementation {
+
+    {
+
+        LinkedList list=new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(1, 15);
+        list.addFirst(5);
+        list.addLast(25);
+        list.offer(30);
+        list.offerFirst(1);
+        list.offerLast(35);
+
+        System.out.println(list.get(2));
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println(list.peek());
+        System.out.println(list.peekFirst());
+        System.out.println(list.peekLast());
+        list.set(2, 99);
+        list.remove();
+        list.remove(2);
+        list.removeFirst();
+        list.removeLast();
+        list.poll();
+        list.pollFirst();
+        list.pollLast();
+        System.out.println(list.contains(25));
+        System.out.println(list.indexOf(25));
+        System.out.println(list.lastIndexOf(25));
+        list.push(100);
+        System.out.println(list.pop());
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        list.clear();
+    }
+}
